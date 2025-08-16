4db75855feba5a81aa3f062a520774fa45b2a46e
Samuel Huber <samuel@dtech.vision>
2025-08-16 19:43:17 +0200

    enforce brackets in control statements for readability

--- eslint.config.js
+++ eslint.config.js
@@ -81,6 +81,7 @@ export default [
       "eqeqeq": "error",
       "prefer-template": "error",
       "sort-vars": "error",
+      "curly": "error",
 
       "codegen/codegen": "error",
       "no-fallthrough": "off",
